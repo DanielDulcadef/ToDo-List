c6a35aabea77bcf71dbfd67d5c9dfaccc129dcae
DanielDulcadef <dulcalito3@gmail.com>
2022-12-02 10:54:13 +0100

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+
+let numId = 3
+export default function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "HTML", done: false },
+    { id: 2, text: "CSS", done: false },
+    { id: 3, text: "JavaScript", done: false },
+    { id: 4, text: "React JS", done: false }
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <TodoList todos={todos} setTodos={setTodos} />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
 
-export default App;
+function TodoList({ todos, setTodos }) {
+  function handleToggleTodo (todo){
+    const updatedTodos = todos.map((clickedTodo) =>
+    clickedTodo.id === todo.id ? {...clickedTodo, done: !clickedTodo.done}:clickedTodo
+    );
+    setTodos(updatedTodos)
+  
+  }
+  if(!todos.length){
+    return <p>¡No hay mas tareas!</p>
+  }
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li
+        onDoubleClick={() => handleToggleTodo(todo)}
+          style={{
+            textDecoration: todo.done ? "line-through" : ""
+          }}
+          key={todo.id}
+        >
+          {todo.text}
+          <DeleteTodo todo={todo} setTodos = {setTodos} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function DeleteTodo({todo,setTodos}){
+  function handleDeleteTodo(){
+    const confirmed = window.confirm("¿Quieres borrar esta tarea?");
+    if(confirmed){
+      setTodos((prevTodos) =>{
+        return prevTodos.filter((t) => t.id !== todo.id);
+      })
+    }
+  }
+  return(
+    <span onClick={handleDeleteTodo}
+    role="button"
+    style={{
+      color: "green",
+      fontWeight: "bold",
+      marginLeft: 10,
+      cursor: "pointer"
+    }}
+    >
+      🗸
+      </span>
+  );
+}
+
+
+function AddTodo({ setTodos }) {
+  const inputRef = React.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    numId ++;
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id:numId,
+      text,
+      done: false
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+    inputRef.current.value = "";
+  }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add..." ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
